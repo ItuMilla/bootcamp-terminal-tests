f636ae9fee4f32a66d6d78efb30774bfe3bf3a5d
ItuMilla <itumelenglephondo94@gmail.com>
2022-08-22 11:44:00 +0200

    git commit -m "bootcam-terminal-tests "

---
+++ test/isFromBellville.test.js
@@ -0,0 +1,23 @@
+import assert from "assert";
+import greet from "../isFromBellville.js";
+
+describe('The isFromBellville function', function() {
+   
+   
+    it('Should return TRUE if reg starts with CY', function() {
+       var reg = 'CY 123';
+       assert.equal(isFromBellville(reg), true);
+    });
+ 
+    //Error path
+    it('Should return false if Reg does not start with CY', function() {
+       var reg = 'CA 123';
+       assert.equal(isFromBellville(reg), false);
+    });
+    
+    it('Should return TRUE if reg starts with "Cy" as mixed cases.', function() {
+       var reg = 'Cy 123';
+       assert.equal(isFromBellville(reg), true);
+    });
+ 
+ });
